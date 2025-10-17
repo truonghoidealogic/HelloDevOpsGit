8af9c4b079c7ad547d7c4814af64dc2eb1285eac
Truong Ho Duc <truong.ho@idealogic.com.vn>
2025-10-17 09:21:02 +0700

    Add project files.

---
+++ HelloDevOpsGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloDevOpsGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
